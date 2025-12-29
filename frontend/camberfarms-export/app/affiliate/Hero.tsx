b96ddbe8bec949376b83e46f78b8511e728388b0
Paul Saje <paul@briskita.com>
2025-12-29 19:28:56 +0100

    added affiliate page

---
+++ frontend/camberfarms-export/app/affiliate/Hero.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import farmerImg from '../assets/img/farm-lady.webp'
+
+export default function Hero() {
+	return (
+		<section className="w-full h-[60vh] flex flex-col items-center justify-center px-6 py-10 bg-black/70 text-white relative">
+			<Image
+				src={farmerImg}
+				fill
+				sizes="100vw"
+				placeholder="blur"
+				alt="a farmer"
+				className="object-cover object-center lg:object-fill"
+			/>
+			<div
+				className="w-full h-full absolute inset-0 z-1 bg-black/50"
+				aria-hidden
+			/>
+			<div className="w-full sm:w-4/5 md:max-w-3xl flex flex-col items-center justify-center mx-auto mt-10 gap-2 relative z-3">
+				<h1 className="font-poppins font-bold text-center uppercase text-3xl md:text-4xl lg:text-[46px] leading-9 md:leading-12">
+					affiliate program
+				</h1>
+				<p className="font-inter text-base text-center mt-2 text-light-grey max-w-2xl mx-auto">
+					Join Our Network of Global Agro-Export Brokers and Affiliate Marketers
+					today and earn for yourself!
+				</p>
+			</div>
+		</section>
+	)
+}
+
+// TODO fix stretched background image (on desktop)
