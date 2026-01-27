e1ecf47b91b5dcb366e7d2043a285286d4fd5937
Mbonu Osi <axleositech@gmail.com>
2026-01-27 09:53:14 +0100

    make images load faster

--- frontend/africa/app/[locale]/components/Hero.tsx
+++ frontend/africa/app/[locale]/components/Hero.tsx
@@ -1,23 +1,32 @@
 import Navbar from "./Navbar"
 import HeroMain from "./HeroMain"
+import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="relative h-fit w-full bg-[url('/images/hero-bg.png')] bg-cover bg-center flex flex-col justify-center">
-  {/* Gradient overlay covering the entire image */}
-  <div className="absolute inset-0 bg-linear-to-b from-black/70 via-black/40 to-black/70" />
+    <div className="relative w-full h-screen">
+      <Image
+        src="/images/hero-bg.png"
+        alt="Hero background"
+        fill
+        priority
+        quality={80}
+        className="object-cover"
+      />
 
-  {/* Content stays above the gradient */}
-  <div className="relative z-10">
-    <div className="hidden xl:block">
-          <Navbar logoSrc="/images/logo.png"/>
+      {/* Gradient overlay */}
+      <div className="absolute inset-0 bg-linear-to-b from-black/70 via-black/40 to-black/70" />
+
+      {/* Content */}
+      <div className="relative z-10">
+        <div className="hidden xl:block">
+          <Navbar logoSrc="/images/logo.png" />
         </div>
-    <div className="flex pb-56 w-full">
-        <HeroMain />
+        <div className="flex pb-56 w-full">
+          <HeroMain />
+        </div>
+      </div>
     </div>
-    
-  </div>
-</div>
 
   )
 }
